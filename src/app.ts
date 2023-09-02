e7b4b4970613afb663a4430d4c947c089dd4c5f3
Joseph Jophy <79068629+josephjophy@users.noreply.github.com>
2023-09-02 11:38:43 -0500

    Attempted to establish connection with Cassandra cluster

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,51 @@
 // src/app.ts
-import express from 'express';
+const express = require('express');
+const { Client } = require('cassandra-driver');
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello world!');
-});
-
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
+});
+
+
+// Cassandra Configuration
+const cassandraClient = new Client({
+  contactPoints: ['localhost'], // Change this to your Cassandra cluster contact points
+  localDataCenter: 'datacenter1', // Change this to your data center
+  keyspace: 'your_keyspace', // Change this to your keyspace
+});
+
+// Connect to Cassandra
+cassandraClient.connect()
+  .then(() => {
+    console.log('Connected to Cassandra');
+
+    // Start your Express routes here
+    app.get('/', (req, res) => {
+      // Example route
+      res.send('Hello, Cassandra!');
+    });
+
+    // Listen on the specified port
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Error connecting to Cassandra:', error);
+  });
+
+// Gracefully shut down the Cassandra connection when the application exits
+process.on('SIGINT', () => {
+  cassandraClient.shutdown()
+    .then(() => {
+      console.log('Cassandra connection closed.');
+      process.exit(0);
+    })
+    .catch((error) => {
+      console.error('Error closing Cassandra connection:', error);
+      process.exit(1);
+    });
 });

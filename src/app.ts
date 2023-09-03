6e3f3ef5094c807f520ad06512430789051a2c1a
Joseph Jophy <79068629+josephjophy@users.noreply.github.com>
2023-09-03 17:39:30 -0500

    created connection to cassandra db

--- src/app.ts
+++ src/app.ts
@@ -3,49 +3,32 @@ const express = require('express');
 const { Client } = require('cassandra-driver');
 
 const app = express();
-const port = 3000;
+const port = 3003;
+
+import { Request, Response } from 'express';
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
 
+app.get('/', (re: Request, res: Response) => {
+  res.send('Hello world!');
+});
 
 // Cassandra Configuration
-const cassandraClient = new Client({
-  contactPoints: ['localhost'], // Change this to your Cassandra cluster contact points
-  localDataCenter: 'datacenter1', // Change this to your data center
-  keyspace: 'your_keyspace', // Change this to your keyspace
+const client = new Client({
+  contactPoints: ['localhost', '172.17.0.2'], 
+  localDataCenter: 'datacenter1', 
+  keyspace: 'university_of_houston',
 });
 
-// Connect to Cassandra
-cassandraClient.connect()
+// Connect to the Cassandra cluster
+client.connect()
   .then(() => {
-    console.log('Connected to Cassandra');
-
-    // Start your Express routes here
-    app.get('/', (req, res) => {
-      // Example route
-      res.send('Hello, Cassandra!');
-    });
-
-    // Listen on the specified port
-    app.listen(port, () => {
-      console.log(`Server is listening on port ${port}`);
-    });
+    console.log('Connected to Cassandra cluster');
   })
-  .catch((error) => {
-    console.error('Error connecting to Cassandra:', error);
+  .catch((err: Error) => {
+    console.error('Error connecting to Cassandra:', err);
   });
 
-// Gracefully shut down the Cassandra connection when the application exits
-process.on('SIGINT', () => {
-  cassandraClient.shutdown()
-    .then(() => {
-      console.log('Cassandra connection closed.');
-      process.exit(0);
-    })
-    .catch((error) => {
-      console.error('Error closing Cassandra connection:', error);
-      process.exit(1);
-    });
-});
+  
